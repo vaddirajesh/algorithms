ec0d7856037335d98c90263d7dd270eb14736444
vaddirajesh <ravaddi@bizopsol.com>
2016-04-19 21:43:10 -0400

    added two sum solution

--- two_sum/Solution.java
+++ two_sum/Solution.java
@@ -1,10 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Solution {
-      public int[] twoSum(int[] nums, int target) {
-      
-      }
-      public static void main(String[] args) throws Exception{
+ public int[] twoSum(int[] nums, int target) {
+   Map<Integer, Integer> numbers = new HashMap<>();
+   int[] result = new int[2];
+   for(int i=0;i<nums.length;i++){
+     if(numbers.containsKey(nums[i])){
+       int indexOfCorrespondingNumber = numbers.get(nums[i]);
+       result[0]=indexOfCorrespondingNumber;
+       result[1]=i;
+       break;
+     }else{
+       numbers.put(target-nums[i],i);
+     }
+   }
+   return result;
+ }
+  public static void main(String[] args) throws Exception{
           BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
           String inputNumbers = br.readLine();
           String target = br.readLine();

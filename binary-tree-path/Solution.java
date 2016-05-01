253f434b7aa5a35b38ebcfbeee3140c814fb88fc
vaddirajesh <ravaddi@bizopsol.com>
2016-05-01 11:41:39 -0700

    added binary tree path solution

--- binary-tree-path/Solution.java
+++ binary-tree-path/Solution.java
@@ -8,12 +8,33 @@ public class Solution {
     StringBuffer intermediatePath = new StringBuffer();
     Stack<TreeNode> stack = new Stack<>();
     stack.push(root);
-    intermediatePath.append(root.val).append("->");
+    intermediatePath.append(root.val);
     return findBinaryTreePaths(root,intermediatePath,pathList,stack);
   }
 
   public List<String> findBinaryTreePaths(TreeNode node, StringBuffer intermediatePath, List<String> pathList, Stack<TreeNode> stack){
-
+    if(node.left ==null && node.right==null){
+      pathList.add(intermediatePath.toString());
+      int index = intermediatePath.lastIndexOf("-");
+      intermediatePath.delete(index,intermediatePath.length());
+      stack.pop();
+    }
+    if(node.left!=null){
+      TreeNode newNode = node.left;
+      if(stack.search(newNode)!=1){
+        stack.push(newNode);
+        intermediatePath.append("->").append(newNode.val);
+      }
+      findBinaryTreePaths(newNode,intermediatePath,pathList,stack);
+    }
+    if(node.right!=null){
+      TreeNode newNode = node.right;
+      if(stack.search(newNode)!=1){
+        stack.push(newNode);
+        intermediatePath.append("->").append(newNode.val);
+      }
+      findBinaryTreePaths(newNode,intermediatePath,pathList,stack);
+    }
 
     return pathList;
   }

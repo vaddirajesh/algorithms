14b5d8c47a3335936595f71256ecb61896d4ee44
vaddirajesh <ravaddi@bizopsol.com>
2016-05-13 16:53:31 -0700

    added symbol tables data strucutre

---
+++ symbolTables/BST.java
@@ -0,0 +1,200 @@
+public class BST<Key extends Comparable<Key>, Value>{
+  /**
+   *
+   *
+   *
+   *
+   * */
+  private class Node{
+    private Key key;
+    private Value value;
+    private int count;
+    private Node left, right;
+
+    public Node(Key key , Value value){
+      this.key = key;
+      this.value = value;
+    }
+  }
+  private Node root;
+  /**
+   *
+   *
+   *
+   * */
+  public Value get(Key key){
+    Node x = root;
+    while (x!=null){
+      int comp = key.compareTo(root.key);
+      if(comp > 0) x = x.right;
+      else if(comp <0) x  = x.right;
+      else return x.value;
+    }
+    return null;
+  }
+  /**
+   *
+   *
+   *
+   *
+   * */
+  public void put(Key key, Value value){
+    root = put(root, key ,value);
+  }
+  /**
+   *
+   *
+   *
+   *
+   * */
+  public void delete(Key key){
+    root = delete(root, key);
+  }
+  /**
+   *
+   *
+   *
+   * */
+  private Node delete(Node root, Key key){
+  //case when node is null
+    if(root==null) return null;
+    int comp = key.compareTo(root.key);
+    //if key is less than root
+    if(comp<0)root.left = delete(root.left,key);
+    //if key is greater than root
+    else if(comp>0)root.right= delete(root.right,key);
+    //if key is eqaul to root node, first save the reference
+    //to root in a temp variable, find the min in right subtree,
+    //set that as root. right subtree will be the remiaing treen
+    //after deleting min from the original right tree and left subtree
+    //of new root is same as old  root
+    else{
+      if(root.left==null) return root.right;
+      if(root.right==null) return root.left;
+      Node temp = root;
+      root = min(root.right);
+      root.right = deleteMin(temp.right);
+      root.left = temp.left;
+    }
+    root.count = 1+size(root.left)+size(root.right);
+    return root;
+  
+  }
+  /**
+   *
+   *
+   * */
+  public void deleteMin(){
+    root = deleteMin(root);
+  }
+  /**
+   *
+   *
+   *
+   * */
+  public Key Floor(Key key){
+    Node node = floor(root, key);
+    if(node==null) return null;
+    return node.key;
+  }
+  /**
+   *
+   *
+   * */
+  public Iterable<Key> iterator(){
+    Queue<Key> queue = new Queue<>();
+    inorder(root,queue);
+    return queue;
+  }
+  /**
+   *
+   *
+   *
+   * */
+  public int size(){
+    return size(root);
+  }
+  /**
+   *
+   *
+   * */
+  public int rank(Key key){
+    return rank(root,key);
+  }
+  /**
+   *
+   *
+   *
+   **/ 
+  private Node put(Node root, Key key , Value value){
+      if(root==null) return new Node(key, value);
+      int comp = key.compareTo(root.key);
+      if(comp < 0)
+        root.left = put(root.left,key,value);
+      else if(comp > 0)
+        root.right = put(root.right,key,value);
+      else
+        root.value = value;
+      root.count = 1+size(root.left)+size(root.right);
+      return root;
+  }
+  /**
+   *Floor takes in key and the root node to find the largest
+   *node which has largest key less than the given key
+   *Tricky part is when we check for the key in the right subtree
+   *We find if there exists some node which is the largest node key less 
+   * than given key : If no such node is found return the current node else
+   * return the computed node
+   * */
+  private Node floor(Node root, Key key){
+    if(root==null) return null;
+    int comp = key.compareTo(root.key);
+    if(comp<0)
+      return floor(root.left, key);
+    else if(comp==0)return root;
+    Node node = floor(root.right, key);
+    if(node==null)
+      return root;
+    else 
+      return node;
+  }
+  /**
+   *
+   *
+   * */
+  private int size(Node node){
+    if(node==null) return 0;
+    return node.count;
+  }
+  /**
+   *
+   *
+   * */
+  private int rank(Node root, Key key){
+    if(root==null) return 0;
+    int comp = key.compareTo(root.key);
+    if(comp<0) return rank(root.left,key);
+    if(comp>0) return 1+size(root.left)+rank(root.right, key);
+    else return size(root.left);
+  }
+  /**
+   *
+   *
+   * */
+  private void inorder(Node root, Queue<Key> queue){
+    if(root==null) return queue;
+    inorder(root.left, queue);
+    queue.enqueue(root.key);
+    inorder(root.right,queue);
+  }
+  /**
+   *
+   *
+   * */
+  private Node deleteMin(Node root){
+    if(root.left==null) return root.right;
+    root.left = deleteMin(root.left);
+    root.count= 1+size(root.left)+size(root.right);
+    return  root;
+  }
+}
---
+++ symbolTables/ST.java
@@ -0,0 +1,10 @@
+public class ST<Key, Value>{
+  public void ST(){}
+  void put(Key key, Value value){}
+  Value get(Key key){}
+  void delete(Key key){}
+  boolean contains(Key key){}
+  boolean isEmpty(){}
+  int size(){}
+  Iterable<Key> keys(){}
+}
